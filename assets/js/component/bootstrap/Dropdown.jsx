d2e67cadb30aacf9cee99ab273dc7a73509cc56f
LingXiao <lingxiao8@126.com>
2015-07-09 23:25:17 +0800

    完成组件基本功能

--- assets/js/component/bootstrap/Dropdown.jsx
+++ assets/js/component/bootstrap/Dropdown.jsx
@@ -1,3 +1,5 @@
+"use strict";
+
 var React = require("react"),
     $ = require("jquery"),
     CSSTransitionGroup = require("../react/CSSTransitionGroup/CSSTransitionGroup.js");
@@ -12,13 +14,15 @@ var Dropdown = React.createClass({
         type: React.PropTypes.string,
         icon: React.PropTypes.string,
         clickMenuClose: React.PropTypes.bool,
+        readOnly: React.PropTypes.bool,
         children: React.PropTypes.node.isRequired
     },
     getDefaultProps: function () {
         return {
             type: "button",  //input OR button
             icon: "fa fa-caret-down",
-            clickMenuClose: true
+            clickMenuClose: true,
+            readOnly: false
         };
     },
 
@@ -67,7 +71,7 @@ var Dropdown = React.createClass({
         if(this.props.type === "button"){
           return (
               <span className={"dropdown" + (this.state.show ? " open" : "")}>
-                  <button className={this.props.className}  onClick={this.toggleShow}>
+                  <button className={this.props.className}  onClick={this.toggleShow} disabled={this.props.readOnly}>
                     {this.props.value + " "}
 										<i className={this.props.icon}></i>
 									</button>
@@ -77,7 +81,7 @@ var Dropdown = React.createClass({
         }else{
           return (
               <span className={"dropdown block input-icon input-icon-right" + (this.state.show ? " open" : "")}>
-                  <input className={this.props.className}  type="text" value={this.props.value}/>
+                  <input className={this.props.className}  type="text" value={this.props.value} disabled={this.props.readOnly}/>
                   <i className={this.props.icon} onClick={this.toggleShow}></i>
                   {child}
               </span>

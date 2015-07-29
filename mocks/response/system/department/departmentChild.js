9b545a501c00a3fb052c5c2e83c181b32628b807
Zongwei <zongwei007@gmail.com>
2015-07-29 20:25:17 +0800

    使用国美组织结构数据

--- mocks/response/system/department/departmentChild.js
+++ mocks/response/system/department/departmentChild.js
@@ -1,25 +1,30 @@
-var Mock = require("mockjs");
+var _ = require("underscore"),
+  departments = require("../../../static/department");
+
+var departments = _.chain(departments)
+  .sortBy(function (item) {
+    return item.sort;
+  })
+  .map(function (item) {
+    item.size = _.where(departments, {
+      parent: item.objectId
+    }).length;
+
+    return item;
+  }).value();
 
 module.exports = function (data) {
 
-  var result = {},
-    template = {};
-  var deptId = decodeURIComponent(data.params.deptId);
-  var top = 0;
+  var result = {};
+  var deptId = decodeURI(data.params.deptId);
 
   if(deptId === "@root"){
     deptId = null;
-    top = 3;
   }
 
-  template["data|" + top + "-10"] = [{
-    "objectId": "@string('lower' + 'upper' + 'number', 24)",
-    "name": "@region",
-    "size|0-10": 1,
-    "parent": deptId
-  }];
-
-  result.json = Mock.mock(template).data;
+  result.json = _.filter(departments, function (item) {
+    return item.parent === deptId;
+  });
 
   return result;
 };

dd26de4b96833b9d7b455a366ba71570bf3d0750
Zongwei <zongwei007@gmail.com>
2015-07-30 19:17:03 +0800

    完成组织树的单/多选功能

--- assets/js/component/form/OrganizationTree.jsx
+++ assets/js/component/form/OrganizationTree.jsx
@@ -34,17 +34,66 @@ const OrganizationTree = React.createClass({
   },
   getInitialState() {
     return {
-      cache: {
-        [ROOT_CODE]: []
-      },
-      data: []
+      store: {},
+      value: this.props.value,
+      checked: this.getChecked()
     };
   },
   componentWillMount() {
     action.children('@root').then(resp => {
-      this.state.cache[ROOT_CODE] = resp;
-      this.forceUpdate();
+      this.setState({
+        store: {
+          [ROOT_CODE]: resp
+        }
+      });
     });
+  },
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      value: nextProps.value,
+      checked: this.getChecked({}, nextProps.value)
+    });
+  },
+  _checkParent(checed, parentId) {
+    if(parentId === null){
+      return;
+    }
+
+    checked[parentId] = 1;
+    if(this.state.store[parentId]){
+      let parent;
+      _.some(this.state.store, (resp) => {
+        parent = _.findWhere(resp, {
+          objectId: parentId
+        });
+        if (parent) {
+          return true;
+        }
+      });
+
+      if(parent && parent.parent){
+        this._checkParent(checked, parent.parent);
+      }
+    }
+
+    return checked;
+  },
+  getChecked(checked = {}, value = this.props.value) {
+    _.forEach(value, (item) => {
+      checked[item.objectId] = 2;
+
+      if(this.props.mult){
+        if (!checked[item.parent]) {
+          this._checkParent(item.parent);
+        }
+
+        if (this.state.store[item.objectId]) {
+          this.getChecked(checked, this.state.store[item.objectId]);
+        }
+      }
+    });
+
+    return checked;
   },
   toggleAll(open) {
     _.forEach(this.refs, function (ref) {
@@ -60,13 +109,20 @@ const OrganizationTree = React.createClass({
 
     return list;
   },
-  handleChange(data) {
-    if (this.props.onChange) {
-      if (this.props.mult) {
-        this.props.onChange(this.getValue());
-      } else {
-        this.props.onChange([data]);
-      }
+  handleChange(item) {
+    if(!this.props.mult){
+      this.setState({
+        value: [item],
+        checked: {
+          [item.objectId]: 2
+        }
+      }, () => {
+        if (this.props.onChange) {
+          this.props.onChange(this.getValue());
+        }
+      });
+    } else if (this.props.onChange) {
+      this.props.onChange(this.getValue());
     }
   },
   onClick(item) {
@@ -78,9 +134,16 @@ const OrganizationTree = React.createClass({
     let handle = $(event.target).closest('.tree-folder-header');
     if (handle.size() === 1 && handle.data('id')) {
       let id = handle.data('id');
-      if (!this.state.cache[id]) {
+      if (!this.state.store[id]) {
         action.children(id).then(resp => {
-          this.state.cache[id] = resp;
+          this.state.store[id] = resp;
+
+          if(this.props.mult && this.state.checked[id] === 2){
+            _.forEach(resp, (item) => {
+              this.state.checked[item.objectId] = 2;
+            });
+          }
+
           this.forceUpdate();
         });
       }
@@ -88,11 +151,11 @@ const OrganizationTree = React.createClass({
   },
   render() {
     let { selectAble, name } = this.props;
-    let Items = this.state.cache[ROOT_CODE].map((item, i) => {
+    let Items = (this.state.store[ROOT_CODE] || []).map((item, i) => {
       return (
-        <Item selectAble={selectAble} data={item} key={item.objectId}
+        <Item selectAble={selectAble} mult={this.props.mult} data={item} key={item.objectId}
           onClick={this.onClick} onStatusChange={this.handleChange}
-          ref={i} store={this.state.cache} value={this.props.value}/>
+          ref={i} store={this.state.store} checked={this.state.checked}/>
       );
     });
 
@@ -112,32 +175,28 @@ export default OrganizationTree;
 let Item = React.createClass({
   propTypes: {
     selectAble: React.PropTypes.bool,
+    mult: React.PropTypes.bool,
     store: React.PropTypes.object,
     data: React.PropTypes.object,
     onClick: React.PropTypes.func,
     onStatusChange: React.PropTypes.func,
     open: React.PropTypes.bool,
-    value: React.PropTypes.any
+    checked: React.PropTypes.object
   },
   getInitialState() {
-    return {
+    return _.extend({
       open: this.props.open,
-      status: this.checkStatus(this.props.value)
-    }
+      status: this.checkStatus()
+    });
   },
   componentWillReceiveProps(nextProps) {
-    let curStatus = this.state.status,
-      nextStatus = this.checkStatus(nextProps.value);
-    if(curStatus != nextStatus){
-      this.setState({
-        status: nextStatus
-      });
+    let status = this.checkStatus(nextProps.checked);
+    if (this.getStatus() !== status) {
+      this.setState({ status });
     }
   },
-  checkStatus(value) {
-    return _.findWhere(value, {
-      objectId: this.props.data.objectId
-    }) ? 2 : 0;
+  checkStatus(checked = this.props.checked) {
+    return checked[this.props.data.objectId] || 0;
   },
   toggle(event) {
     this.setState({
@@ -149,7 +208,7 @@ let Item = React.createClass({
     this.setState({ open });
     _.forEach(this.refs, function (ref) {
       ref.toggleAll(open);
-    })
+    });
   },
   check() {
     if (this.props.readOnly) {
@@ -158,18 +217,25 @@ let Item = React.createClass({
     let status = this.state.status;
     status = status < 2 ? 2 : 0;
     this.setStatus(status);
-    // setTimeout wait state changed
-    setTimeout(() => {
+
+    _.defer(() => {
       this.props.onStatusChange(this.props.data);
-    }, 0);
+    });
   },
   setStatus(status) {
-    this.setState({ status });
-    if(this.props.mult){
+    if (this.props.mult && status !== 1) {
       _.forEach(this.refs, function (ref) {
         ref.setStatus(status);
       });
     }
+
+    if (status) {
+      this.props.checked[this.props.data.objectId] = status;
+    } else {
+      delete this.props.checked[this.props.data.objectId]
+    }
+
+    this.setState({ status });
   },
   getStatus() {
     return this.state.status;
@@ -181,12 +247,12 @@ let Item = React.createClass({
   },
   // 通过事件向上传播，检查所有子级是否已被选中
   updateStatus(data) {
-    if(this.props.mult){
-      let status;
-      _.some(this.refs, function (ref) {
+    if (this.props.mult) {
+      let status = null;
+      _.some(this.refs, (ref) => {
         let s = ref.getStatus();
-        if (!status) {
-          status = s
+        if (status === null) {
+          status = s;
           if (status === 1) {
             return true;
           }
@@ -195,24 +261,28 @@ let Item = React.createClass({
           return true;
         }
       });
-      this.setState({ status });
-      this.props.onStatusChange(data);
+
+      this.setStatus(status);
     }
+
+    this.props.onStatusChange(data);
   },
   getChecked(list, greedy) {
-    if (this.state.status >= (greedy ? 1 : 2)) {
+    if (this.getStatus() >= (greedy ? 1 : 2)) {
       list.push(this.props.data);
     }
 
-    _.forEach(this.refs, function (ref) {
-      ref.getChecked(list, greedy);
-    });
+    if(true && this.props.data.size > 0 && this.getStatus() !== 2){
+      _.forEach(this.refs, function (ref) {
+        ref.getChecked(list, greedy);
+      });
+    }
   },
   render() {
     let children,
     SelectHandle = "";
 
-    let { name, data, selectAble, readOnly, open, value } = this.props;
+    let { name, data, selectAble, readOnly, open, checked } = this.props;
 
     if(selectAble){
       SelectHandle = this.props.mult ? (
@@ -231,9 +301,9 @@ let Item = React.createClass({
       let childrenData = this.props.store[this.props.data.objectId] || [],
         Items = childrenData.map((item, i) => {
           return (
-            <Item selectAble={selectAble} data={item} key={item.objectId}
+            <Item selectAble={selectAble} data={item} key={item.objectId} mult={this.props.mult}
               onClick={this.onClick} onStatusChange={this.updateStatus} store={this.props.store}
-              open={open} readOnly={readOnly} ref={i} value={value}/>
+              open={open} readOnly={readOnly} ref={i} checked={checked}/>
           )
         });
 

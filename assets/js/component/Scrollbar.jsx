6877415c9de01f4f90e874ae607ad9c66e2c7700
LingXiao <lingxiao8@126.com>
2015-08-05 13:29:47 +0800

    增加一个滚动条组件,待优化

---
+++ assets/js/component/Scrollbar.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import $ from 'jquery';
+import ScrollBar from 'fixed-data-table/internal/Scrollbar.react.js';
+
+const Scrollbar = React.createClass({
+  getInitialState() {
+    return {
+      top: 0,
+      left: 0,
+      containerHeiget: 500,
+      contentHeiget: 800,
+      containerWidth: 1000,
+      contentWidth: 1400
+    };
+  },
+  componentDidMount() {
+    let $node = $(this.getDOMNode());
+    let $content = $node.find("div:first");
+    let state = { };
+    //$node.height($content.height());
+    //$node.width($content.width());
+
+    //$content.height("auto");
+    //$content.width("auto");
+
+    // state.contentHeiget = $content.height();
+    // state.contentWidth = $content.width();
+    //
+    // state.containerHeiget = $node.height();
+    // state.containerWidth = $node.width();
+
+    // this.setState(state);
+  },
+  scrollY(scrollPos) {
+    this.setState({
+      top:-scrollPos
+    })
+  },
+  scrollX(scrollPos) {
+    this.setState({
+      left:-scrollPos
+    })
+  },
+  render() {
+    let style = {
+      top: this.state.top + "px",
+      left: this.state.left + "px",
+      position: "relative"
+    }
+
+    return (
+      <div className="custom-scrollbar">
+        <div {...this.props} style={style}>
+          {this.props.children}
+        </div>
+        <ScrollBar size={this.state.containerHeiget} contentSize={this.state.contentHeiget} onScroll={this.scrollY}/>
+        <ScrollBar orientation="horizontal" size={this.state.containerWidth} contentSize={this.state.contentWidth} onScroll={this.scrollX} />
+      </div>
+    );
+  }
+
+});
+
+export default Scrollbar;

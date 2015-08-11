3105d32ca27c09ad19ad9babab1d92a8c43cd347
Zongwei <zongwei007@gmail.com>
2015-08-11 13:26:38 +0800

    实现FieldSet的区块只读

--- assets/js/component/form/Fieldset.jsx
+++ assets/js/component/form/Fieldset.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-const Fieldset = React.createClass({
-  propTypes: {
-    className: React.PropTypes.any,
-    title: React.PropTypes.string
-  },
+export default class Fieldset extends React.Component {
+  static propTypes = {
+    className: PropTypes.string,
+    title: PropTypes.string,
+    readOnly: PropTypes.bool
+  }
   render() {
     return (
       <fieldset className={this.props.className}>
-        { this.props.title ? (
-        <legend>{this.props.title}</legend>
-        ): null }
-        { this.props.children }
+        {this.props.title ? (
+          <legend>{this.props.title}</legend>
+        ) : null }
+        {React.Children.map(this.props.children, (child) => {
+          return React.cloneElement(child, {
+            readOnly: child.props.readOnly || this.props.readOnly || undefined
+          });
+        })}
       </fieldset>
     );
   }
-});
-
-export default Fieldset;
+};

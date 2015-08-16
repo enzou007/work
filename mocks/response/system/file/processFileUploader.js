77ff825e5bfe1d6d33a4806d0dbd7116c604d86c
LingXiao <lingxiao8@126.com>
2015-08-16 12:17:28 +0800

    修改附件上传请求监听方式,在gulpfile配置中单独处理.

--- mocks/response/system/file/processFileUploader.js
+++ mocks/response/system/file/processFileUploader.js
@@ -2,8 +2,6 @@ var fs = require("fs");
 var formidable = require("formidable");
 var Mock = require("mockjs");
 
-var ROOT_PATH = __dirname.replace(/mocks.*$/, "").replace(/\\/g, "/");
-
 var BASE_PATH = "build/atts/";
 var BASE_PATH_TMP = "build/atts/tmp/"
 
@@ -14,32 +12,40 @@ if(!fs.existsSync(BASE_PATH)){
 if(!fs.existsSync(BASE_PATH_TMP)){
   fs.mkdirSync(BASE_PATH_TMP);
 }
-module.exports = function (data, req, res) {
+module.exports = function (req, res, next) {
+  if(req.method.toUpperCase() === "POST" && req.url.toLowerCase().indexOf("/1/system/filesystemserver") > -1){
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    var statusCode = 200;
+    var result = { info: "ok" }
 
-  var form = new formidable.IncomingForm();
-  var path = "";
-  if(data.params && data.params.objectId){
-    path = BASE_PATH + data.params.objectId;
+    var objectId = req.url.match(/filesystemserver\/{0,1}(.*)$/i)[1];
+    var form = new formidable.IncomingForm();
+    var path = "";
+    if(objectId){
+      path = BASE_PATH + objectId;
+    }else{
+      path = BASE_PATH_TMP + Mock.Random.guid();
+    }
+
+    if(!fs.existsSync(path)){
+      fs.mkdirSync(path);
+    }
+
+    path += "/";
+
+    form.uploadDir = path;
+
+    var filePath = path;
+
+    form.parse(req, function(err, fields, files){
+      filePath += files.file.name;
+      fs.rename(files.file.path, filePath);
+      res.statusCode = statusCode;
+      result.originPath = filePath.replace("build", "");
+      res.end(JSON.stringify(result));
+      next();
+    });
   }else{
-    path = BASE_PATH_TMP + Mock.Random.guid();
-  }
-  if(!fs.existsSync(path)){
-    fs.mkdirSync(path);
-  }
-
-  path += "/";
-
-  form.uploadDir = path;
-
-  var filePath = path;
-
-  form.parse(req, function(err, fields, files){
-    filePath += files.file.name;
-    fs.rename(files.file.path, filePath);
-  });
-  console.log(filePath);
-  return {
-    json: {"originPath": filePath.replace("build/","")},
-    status: 200
+    next()
   }
 }
